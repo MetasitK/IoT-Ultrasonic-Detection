83f6ae128316dc66bc22f146e0e62c805b3e3ffc
Metasit Getrak <metasit.get@gmail.com>
2023-04-29 15:46:37 +0700

    initial

---
+++ Code/config.h
@@ -0,0 +1,13 @@
+const char* noderedServer = "";
+const int noderedMQTTPort = 1883;
+
+/* NetPie MQTT Server */
+const char* netpieServer = "broker.netpie.io";
+const int netpiePort = 1883;
+const char* netpieClientID = ""; //Client ID
+const char* netpieUser = ""; //Token
+const char* netpiePassword = ""; //Secret
+
+/* (WiFi) Variables */
+char ssid[] = ""; 
+char pass[] = ""; 
